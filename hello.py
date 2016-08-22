a9de2916a62715489c122bcde48b496419cb1812
Pandoraemon <pz880304@163.com>
2016-08-22 10:59:33 +0800

    fix

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('test.html'）
+    return render_template('test.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
