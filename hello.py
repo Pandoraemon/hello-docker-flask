52d856950acce3c03a7b1452b0c6508cc65f9a55
Pandoraemon <pz880304@163.com>
2016-08-22 10:40:02 +0800

    use template of valume

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello DaoCloud</h1>'
+    return render_template('test.html'）
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
